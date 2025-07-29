47ff6d453b8ead0459b2717ef33c9bbb632ed9de
Gustavo Leite <gustavo.jog@hotmail.com>
2025-07-29 19:19:11 -0300

    feat: setup project

---
+++ src/main/java/br/com/fiap/javacleanarch/javacleanarch/JavacleanarchApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.javacleanarch.javacleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacleanarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacleanarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/javacleanarch/javacleanarch/JavacleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.javacleanarch.javacleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacleanarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
